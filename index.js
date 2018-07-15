00e07f039d3024ee39e50c2a9d191c96cc900741
Sergio Gare <sergio.gare.sagg@gmail.com>
2018-07-14 22:52:52 -0500

    Finalizamos las funciones de compresión.
    Ahora se puede comprimir "hashes" y números decimales de una manera simple, también se agrego un archivo de experimentación.

--- index.js
+++ index.js
@@ -1,19 +1,83 @@
-// Created by Sergio Gare at 11/07/2018.
+// Created by Sergio Gare at 14/07/2018.
 
-const to256 = require('./inverse');
-const toDecimal = require('./toDecimal');
+class Sid {
 
-console.log(toDecimal('222', 128));
-console.log(String.fromCharCode(50));
+    constructor (data) {
 
-to256(6450);
+        this.data = data;
+        this.compressed = '';
+        this.decompressed = this.compressed;
 
-/*const stdin = process.openStdin();
+    }
 
-stdin.addListener('data', (data) => {
+    compress10 () {
 
-    digits = data.toString().trim();
+        let final = [];
+        this.data = this.data.toString();
+        let len = this.data.length;
 
-    console.log(toDecimal(digits));
+        for (let i = 0; i < len; i += 4)
 
-});*/
+            final.push(String.fromCharCode(parseInt(this.data.substr(i, 4))));
+
+        this.compressed = final.join('');
+
+        return this.compressed;
+
+    }
+
+    decompress10 () {
+
+        let final = [];
+
+        for (let i = this.compressed.length - 1; i >= 0; i--)
+
+            final.unshift(this.compressed.charCodeAt(i));
+
+        this.decompressed = final.join('');
+
+        return this.decompressed;
+
+    }
+
+    compress16 () {
+
+        // De base 16 a 65536.
+
+        let final = [];
+
+        for (let i = 0; i < 64; i+=4)
+
+            final.push(String.fromCharCode(parseInt(this.data.substr(i, 4), 16)));
+
+        this.compressed = final.join('');
+
+        return this.compressed;
+
+    }
+
+    decompress16 () {
+
+        // De base 65536 a 16.
+
+        let final = [];
+
+        for (let i = this.compressed.length - 1; i >= 0; i--)
+
+            final.unshift(this.compressed.charCodeAt(i).toString(16).padStart(4, '0'));
+
+        this.decompressed = final.join('');
+
+        return this.decompressed;
+
+    }
+
+}
+
+module.exports = Sid;
+
+let crunch = new Sid(1111199999);
+crunch.compress10();
+crunch.decompress10();
+
+console.log(crunch.decompressed);
